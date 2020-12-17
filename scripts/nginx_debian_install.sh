1a75d2fec856bbd307b5da2d46a6fa3eab8867e9
Joseph Diza <josephm.diza@gmail.com>
2020-12-17 11:45:46 -0800

    Add nginx install scripts

---
+++ scripts/nginx_debian_install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# http://nginx.org/en/linux_packages.html
+
+sudo apt install curl gnupg2 ca-certificates lsb-release
+
+# Stable
+#echo "deb http://nginx.org/packages/debian `lsb_release -cs` nginx" \
+        #| sudo tee /etc/apt/sources.list.d/nginx.list
+
+# Mainline Released
+echo "deb http://nginx.org/packages/mainline/debian `lsb_release -cs` nginx" \
+    | sudo tee /etc/apt/sources.list.d/nginx.list
+
+curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo apt-key add -
+sudo apt-key fingerprint ABF5BD827BD9BF62
+sudo apt update
+sudo apt install nginx
