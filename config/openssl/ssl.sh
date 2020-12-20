f7bd20f64886ba9db049ba61e926bde562c8fb97
Joseph Diza <josephm.diza@gmail.com>
2020-12-20 13:21:52 -0800

    Change export_client_key source line

--- config/openssl/ssl.sh
+++ config/openssl/ssl.sh
@@ -4,7 +4,7 @@
 source make_subdirs.sh
 source setup_ssl.sh
 
-source create_ca.sh # Create CertAuth
+source create_ca.sh             # Create CertAuth
 
 source create_server_certs.sh   # Create server certificate
 source ca_sign_server.sh        # Have the CertAuth sign the server's CSR
@@ -12,8 +12,8 @@ source ca_sign_server.sh        # Have the CertAuth sign the server's CSR
 source create_client_certs.sh   # Create the client certificates
 source ca_sign_client.sh        # Have the CertAuth sign the client's CSR
 
-source create_client_key.sh     # Create the client key
+source export_client_key.sh     # Create the client key
+source export_server_key.sh     # Create the server key
 
 # Set up Apache/Nginx for one-way SSL auth
-
 # Set up Apache/Nginx for two-way SSL auth
