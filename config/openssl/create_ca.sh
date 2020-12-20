c55e6c92c46919b23e71b3d41eb4ecaa07460099
Joseph Diza <josephm.diza@gmail.com>
2020-12-20 13:10:35 -0800

    Add comment detailing output files for ca

--- config/openssl/create_ca.sh
+++ config/openssl/create_ca.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Creates ca/cacert.pem private/ca/cakey.pem
+
 OUTPUT="ca"
 
 openssl req -new -x509 -extensions v3_ca -keyout private/$OUTPUT/cakey.pem -out $OUTPUT/cacert.pem -days 365 -config ./openssl.cnf -batch
