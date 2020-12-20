611869091b9c788d75662b21cf8828e17658f8e8
Joseph Diza <josephm.diza@gmail.com>
2020-12-20 12:11:08 -0800

    Enable non-interactive mode for create_ca.sh

--- config/openssl/create_ca.sh
+++ config/openssl/create_ca.sh
@@ -2,5 +2,5 @@
 
 OUTPUT="ca"
 
-openssl req -new -x509 -extensions v3_ca -keyout private/$OUTPUT/cakey.pem -out $OUTPUT/cacert.pem -days 365 -config ./openssl.cnf
+openssl req -new -x509 -extensions v3_ca -keyout private/$OUTPUT/cakey.pem -out $OUTPUT/cacert.pem -days 365 -config ./openssl.cnf -batch
 
