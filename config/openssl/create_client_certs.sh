b36f3eda1246539acc37b3225b21adaf357e6b52
Joseph Diza <josephm.diza@gmail.com>
2020-12-20 12:21:37 -0800

    Batch cert client creation with different CN

--- config/openssl/create_client_certs.sh
+++ config/openssl/create_client_certs.sh
@@ -2,4 +2,4 @@
 
 CSR="csr/client"
 KEY="keys/client"
-openssl req -new -nodes -out $CSR/client-req.pem -keyout private/$KEY/client-key.pem -days 365 -config openssl.cnf 
+openssl req -new -nodes -out $CSR/client-req.pem -keyout private/$KEY/client-key.pem -days 365 -config openssl.cnf -subj "/CN=client" -batch
