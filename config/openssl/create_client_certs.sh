a2e5e615a049f1bcbaa9d73c37aa01a95de0dee9
Joseph Diza <josephm.diza@gmail.com>
2020-12-20 12:23:40 -0800

    Revert batch, CN change in create_client_certs.sh

--- config/openssl/create_client_certs.sh
+++ config/openssl/create_client_certs.sh
@@ -2,4 +2,5 @@
 
 CSR="csr/client"
 KEY="keys/client"
-openssl req -new -nodes -out $CSR/client-req.pem -keyout private/$KEY/client-key.pem -days 365 -config openssl.cnf -subj "/CN=client" -batch
+openssl req -new -nodes -out $CSR/client-req.pem -keyout private/$KEY/client-key.pem -days 365 -config openssl.cnf
+# TODO: Make another openssl.cnf with different client CN
