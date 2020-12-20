6791e197241234b8c67e05e9e921303477004f5c
Joseph Diza <josephm.diza@gmail.com>
2020-12-20 12:16:22 -0800

    Clean up ssl.sh script

--- config/openssl/ssl.sh
+++ config/openssl/ssl.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
 
 source make_subdirs.sh
-
-# Configure ssl
 source setup_ssl.sh
 
-# Create CertAuth
-source create_ca.sh
+source create_ca.sh # Create CertAuth
 
-# Create server certificate
-source create_server_certs.sh
+source create_server_certs.sh   # Create server certificate
+source ca_sign_server.sh        # Have the CertAuth sign the server's CSR
 
-# Have the CertAuth sign the server's CSR
-source ca_sign_server.sh
+source create_client_certs.sh   # Create the client certificates
+source ca_sign_client.sh        # Have the CertAuth sign the client's CSR
 
-# Create the client certificates
-source create_client_certs.sh
-
-# Have the CertAuth sign the client's CSR
-source ca_sign_client.sh
-
-# Create the client key
-source create_client_key.sh
+source create_client_key.sh     # Create the client key
 
 # Set up Apache/Nginx for one-way SSL auth
 
