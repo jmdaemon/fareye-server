c92526748f0dabda3ded96de5f71e17871ec26ae
Joseph Diza <josephm.diza@gmail.com>
2020-12-20 12:16:35 -0800

    Add batch mode to create_server_certs

--- config/openssl/create_server_certs.sh
+++ config/openssl/create_server_certs.sh
@@ -2,4 +2,4 @@
 
 CSR="csr/server"
 KEY="keys/server"
-openssl req -new -nodes -out $CSR/server-req.pem -keyout private/$KEY/server-key.pem -days 365 -config openssl.cnf
+openssl req -new -nodes -out $CSR/server-req.pem -keyout private/$KEY/server-key.pem -days 365 -config openssl.cnf -batch
