fcb6bae63c3abe775167cc37354e86f06b52ac49
Joseph Diza <josephm.diza@gmail.com>
2020-12-20 12:27:34 -0800

    Rename create_client_key.sh -> export_client_key.sh

--- config/openssl/create_client_key.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CA="ca"
-CERT="certs/client"
-KEY="keys/client"
-EXPORT="export/pkcs12"
-openssl pkcs12 -export -in $CERT/client-cert.pem -inkey private/$KEY/client-key.pem -certfile $CA/cacert.pem -name "Client" -out $EXPORT/client-cert.p12 
-
