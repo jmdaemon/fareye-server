3faaab081e0bba618708872dc80308a82172823f
Joseph Diza <josephm.diza@gmail.com>
2020-12-24 15:27:34 -0800

    Run create_client_certs with openssl.client.cnf instead of openssl.cnf

--- config/openssl/create_client_certs.sh
+++ config/openssl/create_client_certs.sh
@@ -2,5 +2,6 @@
 
 CSR="csr/client"
 KEY="keys/client"
-openssl req -new -nodes -out $CSR/client-req.pem -keyout private/$KEY/client-key.pem -days 365 -config openssl.cnf
+#openssl req -new -nodes -out $CSR/client-req.pem -keyout private/$KEY/client-key.pem -days 365 -config openssl.cnf
+openssl req -new -nodes -out $CSR/client-req.pem -keyout private/$KEY/client-key.pem -days 365 -config openssl.client.cnf -batch
 # TODO: Make another openssl.cnf with different client CN
