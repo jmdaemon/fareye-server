ccae39a0d8b5ccd4cce82c976a42de79f4647c43
Joseph Diza <josephm.diza@gmail.com>
2022-03-30 11:34:21 -0700

    Change names of Encryption memvars

--- public/crypt.php
+++ public/crypt.php
@@ -1,34 +1,31 @@
 <?php
 
-class Encryption
-{
-    private $encryptedData;
+class Encryption {
+    private $data;
     private $iv;
-    private $secretKey;
-    const AES = "AES-256-CBC";
+    private $key;
+
+    const AES = 'AES-256-CBC';
     // private const RSA = "";
     // AES-256 Encryption
-    public function setKey($key) { $secretKey = $key; }
-    public function encrypt($data) { return openssl_encrypt($data, "AES-256-CBC", $secretKey, $iv); }
-    // string openssl_decrypt ( string $data , string $method , string $key [, int $options = 0 [, string $iv = "" [, string $tag = "" [, string $aad = "" ]]]] )
-    public function decrypt($data, $iv)
-    {
+
+    // Setters
+    public function setKey($key) { $key = $key; }
+
+    // Getters
+
+    public function encrypt($data) { return openssl_encrypt($data, "AES-256-CBC", $key, $iv); }
+    public function decrypt($data, $iv) {
         //$byte_array = unpack('C*', 'The quick fox jumped over the lazy brown dog');
         //var_dump($byte_array);  // $byte_array should be int[] which can be converted
                                 // to byte[] in C# since values are range of 0 - 255
                                 
-        return openssl_decrypt($encryptedData, "AES-256-CBC", $secretKey, $data, $iv);
+        return openssl_decrypt($data, "AES-256-CBC", $key, $data, $iv);
     }
     
     // RSA 2048 Encryption
-    public function transport()
-    {}
-    
-    public function receive()
-    {}
-    
-    public function passwordChecking()
-    {}
+    public function transport() {}
+    public function receive() {}
+    public function passwordChecking() {}
 }
-
 ?>
