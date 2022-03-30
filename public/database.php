bd9e823f7c6af1d0c7c6e9bf0d905aed030b8729
Joseph Diza <josephm.diza@gmail.com>
2022-03-30 16:36:02 -0700

    Add MySQLDatabase class

--- public/database.php
+++ public/database.php
@@ -3,30 +3,28 @@
 /**
  * PHP MySQL Create Table Demo
  */
-class Users {
 
+class Database {
     /**
     * DB_HOST: Domain of the site
     * DB_NAME: Name of the database
     * DB_USER: Owner of the database
     * DB_PASSWORD: Owner's password to the database
     */
+
     const DB_HOST = 'localhost';
     const DB_NAME = 'id6011112_atm ';
     const DB_USER = 'id6011112_joseph';
     const DB_PASSWORD = '';
 
-    /**
-     *
-     * @var type
-     */
+    // PHP Data Object used for connecting to databases
     private $pdo = null;
+}
 
-    /**
-     * Open the database connection
-     */
+class MySQLDatabase extends Database {
+
+    // Open a database connection
     public function __construct() {
-        // open database connection
         $conStr = sprintf("mysql:host=%s;dbname=%s", self::DB_HOST, self::DB_NAME);
         try {
             $this->pdo = new PDO($conStr, self::DB_USER, self::DB_PASSWORD);
@@ -35,14 +33,11 @@ class Users {
         }
     }
 
-    /**
-     * close the database connection
-     */
-    public function __destruct() {
-        // close the database connection
-        $this->pdo = null;
-    }
+    // Close the database connection
+    public function __destruct() { $this->pdo = null; }
+}
 
+class Users extends MySQLDatabase {
     /**
      * create the tasks table
      * @return boolean returns true on success or false on failure
