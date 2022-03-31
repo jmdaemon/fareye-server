7efa67e0881a2728272a46601a7d49e162d81a13
Joseph Diza <josephm.diza@gmail.com>
2022-03-30 17:29:48 -0700

    Remove dead code in api.php

--- public/api.php
+++ public/api.php
@@ -4,41 +4,29 @@
 $db = new MySQLDatabase;
 
 $request=$_POST['request'];
-$results=NULL;
-$success_message="";
-$fail_message="";
-$sql="";
 
 switch ($request) {
     case "AddAccount":
         $pass   = $_POST['password'];
-        //$bal    = $_POST['balance'];
         $fname  = $_POST['firstName'];
         $mname  = $_POST['middleName'];
         $lname  = $_POST['lastName'];
         $pin    = $_POST['pin'];
-        $sql = "INSERT INTO
+        $query = "INSERT INTO
                 USERS  (fname, mname, lname, balance, pin, history, password)
                 VALUES ($fname, $mname, $lname, 0.0, $pin, '', $password);";
-        $results = mysqli_query($db->getPDO(),$sql);
-        $success_message="Account Creation Successful.";
-        $fail_message="Account Creation Failed.";
+        $results = mysqli_query($db->getPDO(),$query);
         if ($results === TRUE) {
-            echo $success_message;
+            echo "Account Creation Successful.";
         } else {
-            echo $fail_message;
-            echo "Error: " . $sql . "<br>" . $db->getPDO()->error;
+            echo "Account Creation Failed.";
+            echo "Error: " . $query . "<br>" . $db->getPDO()->error;
         }
-
         break;
     case "CheckNumber":
-        $pin    = $_POST['pin'];
-        //$pass   = $_POST['password'];
-        //$fname  = $_POST['firstName'];
-        //$mname  = $_POST['middleName'];
-        //$lname  = $_POST['lastName'];
-        $sql = "SELECT * FROM USERS WHERE pin LIKE $pin";
-        $results = mysqli_query($db->getPDO(),$sql);
+        $pin        = $_POST['pin'];
+        $query      = "SELECT * FROM USERS WHERE pin LIKE $pin";
+        $results    = mysqli_query($db->getPDO(),$query);
         if ($results === FALSE) {
             // User with pin does not exist in our database
             echo "Account number is unique.";
@@ -46,7 +34,7 @@ switch ($request) {
         } else {
             // User with pin exists in our database
             print("Account already associated with a user.");
-            echo "Error: " . $sql . "<br>" . $db->getPDO()->error;
+            echo "Error: " . $query . "<br>" . $db->getPDO()->error;
             return FALSE;
         }
 }
