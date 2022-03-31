8bce58cb72c2a24b2764b456b0b86c647442e18c
Joseph Diza <josephm.diza@gmail.com>
2022-03-30 17:25:42 -0700

    Remove deprecated check_account_number.php

--- public/api.php
+++ public/api.php
@@ -20,16 +20,35 @@ switch ($request) {
         $sql = "INSERT INTO
                 USERS  (fname, mname, lname, balance, pin, history, password)
                 VALUES ($fname, $mname, $lname, 0.0, $pin, '', $password);";
-        $results = mysqli_query($db->getPDO(),$query);
+        $results = mysqli_query($db->getPDO(),$sql);
         $success_message="Account Creation Successful.";
         $fail_message="Account Creation Failed.";
-}
+        if ($results === TRUE) {
+            echo $success_message;
+        } else {
+            echo $fail_message;
+            echo "Error: " . $sql . "<br>" . $db->getPDO()->error;
+        }
 
-if ($results === TRUE) {
-    echo $success_message;
-} else {
-    echo $fail_message;
-    echo "Error: " . $sql . "<br>" . $db->getPDO()->error;
+        break;
+    case "CheckNumber":
+        $pin    = $_POST['pin'];
+        //$pass   = $_POST['password'];
+        //$fname  = $_POST['firstName'];
+        //$mname  = $_POST['middleName'];
+        //$lname  = $_POST['lastName'];
+        $sql = "SELECT * FROM USERS WHERE pin LIKE $pin";
+        $results = mysqli_query($db->getPDO(),$sql);
+        if ($results === FALSE) {
+            // User with pin does not exist in our database
+            echo "Account number is unique.";
+            return TRUE;
+        } else {
+            // User with pin exists in our database
+            print("Account already associated with a user.");
+            echo "Error: " . $sql . "<br>" . $db->getPDO()->error;
+            return FALSE;
+        }
 }
 $db->__destruct;
 ?>
