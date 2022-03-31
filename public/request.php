6da1715dad9a108cd908008059ef721a842af2b4
Joseph Diza <josephm.diza@gmail.com>
2022-03-30 19:48:26 -0700

    Move deposit to request.php

--- public/request.php
+++ public/request.php
@@ -19,14 +19,31 @@ switch ($request) {
     case "add_account":
         $user = parse_user($_POST);
         add_account($pdo, $user);
+        break;
     case "append_history":
         $user = parse_user($_POST);
         $msg = parse_message($_POST);
         logMessage($pdo, $user, $msg);
+        break;
     case "check_pin":
         $user = parse_user($_POST);
         check_pin($pdo, $user);
+        break;
+    case "deposit":
+        $amount = $_POST['amount'];
 
+        $pin     = $_POST['pin'];
+        $amount  = $_POST['amount'];
+        $pass    = $_POST['password'];
+        $balance = $_POST['balance'];
+
+        $user = new User;
+        $user->setPin($pin);
+        $user->setPassword($pass);
+        $user->setBalance($balance);
+
+        deposit($pdo, $amount, $user);
+        break;
 }
 
 ?>
