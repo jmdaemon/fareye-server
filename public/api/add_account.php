863d7b9c3c65c8b41aa23add44c5c0199b3121b1
Joseph Diza <josephm.diza@gmail.com>
2022-03-30 19:45:40 -0700

    Move add_account to requests.php

--- public/api/add_account.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Connect to our database
-$db = new MySQLDatabase;
-$pdo = $db->getPDO();
-
-include('../api.php');
-$user = parse_user($_POST);
-add_account($pdo, $user);
-?>
--- public/request.php
+++ public/request.php
@@ -4,14 +4,22 @@
  * Handle all the Fareye application requests
  */
 
+
+# Imports
+include('../api.php');
+
 // Connect to our database
 $db = new MySQLDatabase;
+$pdo = $db->getPDO();
 
 $request=$_POST['request'];
-$pdo = $db->getPDO();
 
 // Deal with all the requests
 switch ($request) {
+    case "add_account":
+        $user = parse_user($_POST);
+        add_account($pdo, $user);
+
 }
 
 ?>
