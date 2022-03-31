b1bc24f0cfaf9d70858eaf0c5a686e41509657ad
Joseph Diza <josephm.diza@gmail.com>
2022-03-30 18:59:41 -0700

    Create User, move add_account to api.php

--- public/api.php
+++ public/api.php
@@ -22,4 +22,45 @@ function checkResult($success, $fail, $result, $query, $pdo) {
         return FALSE;
     }
 }
+
+/**
+ * Stores information about our user
+ */
+class User {
+    private $fname;
+    private $mname;
+    private $lname;
+    private $pin;
+    private $pass;
+
+    // Getters
+    public function getFirstName()  { return $this->fname; }
+    public function getMiddleName() { return $this->mname; }
+    public function getLastName()   { return $this->lname; }
+    public function getPin()        { return $this->pin; }
+    public function getPassword()   { return $this->pass; }
+
+    // Setters
+    public function setFirstName($fname)  { $this->fname = $fname; }
+    public function setMiddleName($mname) { $this->mname = $mname; }
+    public function setLastName($lname)   { $this->lname = $lname; }
+    public function setPin($pin)          { $this->pin = $pin; }
+    public function setPassword($pass)    { $this->pass = $pass; }
+}
+
+// Adds an account to the database
+function add_account($pdo) {
+    $pass   = $_POST['password'];
+    $fname  = $_POST['firstName'];
+    $mname  = $_POST['middleName'];
+    $lname  = $_POST['lastName'];
+    $pin    = $_POST['pin'];
+    $query = "INSERT INTO
+            USERS  (fname, mname, lname, balance, pin, history, password)
+            VALUES ('$fname', '$mname', '$lname', 0.0, $pin, '', '$pass');";
+    $results = mysqli_query($pdo, $query);
+    return checkResult("Account Creation Successful.", "Account Creation Failed.",
+        $results === TRUE, $query, $pdo);
+
+}
 ?>
--- public/api/add_account.php
+++ public/api/add_account.php
@@ -5,20 +5,7 @@ $db = new MySQLDatabase;
 $request=$_POST['request'];
 $pdo = $db->getPDO();
 
-// Adds an account to the database
-function add_account($pdo) {
-    $pass   = $_POST['password'];
-    $fname  = $_POST['firstName'];
-    $mname  = $_POST['middleName'];
-    $lname  = $_POST['lastName'];
-    $pin    = $_POST['pin'];
-    $query = "INSERT INTO
-            USERS  (fname, mname, lname, balance, pin, history, password)
-            VALUES ('$fname', '$mname', '$lname', 0.0, $pin, '', '$pass');";
-    $results = mysqli_query($pdo, $query);
-    return checkResult("Account Creation Successful.", "Account Creation Failed.",
-        $results === TRUE, $query, $pdo);
+include('../api.php');
 
-}
 add_account($pdo);
 ?>
