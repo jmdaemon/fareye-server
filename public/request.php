5b2fa600a203c70236139af13988a345fc4d4edb
Joseph Diza <josephm.diza@gmail.com>
2022-03-30 19:52:44 -0700

    Move fetch_user, transfer_from to request.php

--- public/request.php
+++ public/request.php
@@ -57,6 +57,29 @@ switch ($request) {
 
         withdraw($pdo, $amount, $user);
         break;
+    case "fetch_user":
+        $user = parse_user($_POST);
+        fetch_user($pdo, $user);
+        break;
+    case "transfer_from":
+        $amount = $_POST['amount'];
+
+        $pin = $_POST['pin'];
+
+        $user = New User;
+        $user->setPin($pin);
+
+        // Initialize Target User
+        $targetPin  = $_POST['targetPin'];
+        $targetPass = $_POST['targetPass'];
+
+        $target = New User;
+        $target->setPin($targetPin);
+        $target->setPassword($targetPass);
+
+        // Transfer funds from target to user
+        transfer_from($pdo, $amount, $target, $user);
+        break;
 }
 
 ?>
